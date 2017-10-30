f13c86b7ef00524e4810f514a50a965737a5bfa9
Thomas Osugi <tosugi@pivotal.io>
2017-10-30 14:53:01 -0600

    convert to redux
    Signed-off-by: Matthew Conger-Eldeen <mcongereldeen@pivotal.io>

--- src/client.js
+++ src/client.js
@@ -1,3 +1,4 @@
+import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
@@ -7,6 +8,19 @@ import * as list from './core/list';
 const url = window.location.href;
 const lastSlashIndex = url.lastIndexOf('/');
 const listId = url.substring(lastSlashIndex + 1);
+
+function newTaskInput(newTaskName) {
+    return {
+        type: 'newTaskInput',
+        newTaskName,
+    }
+}
+
+function newTaskSubmit() {
+    return {
+        type: 'newTaskSubmit',
+    }
+}
 
 function guid() {
     function s4() {
@@ -39,39 +53,63 @@ function loadListEvents(listId, fromVersion = 1, toVersion = 'latest') {
     return fetch(`/${listId}/events?fromVersion=${fromVersion}&toVersion=${toVersion}`).then((res) => res.json())
 }
 
-const render = (commands, events, uiState) => {
+const render = (store) => {
     console.time('render');
-    const onNewTaskInput = (newTaskName) => {
-        render(commands, events, {...uiState, newTaskName: newTaskName});
-    };
-
-    const onNewTaskSubmit = () => {
-        const validationState = events.reduce(
-            (validationState, event) => list.eventHandlers[event.type](event, validationState),
-            list.emptyState()
-        );
-        const addItemCommand = list.addItem({id: guid(), name: uiState.newTaskName});
-        const {type, v} = list.commandHandlers.addItem(addItemCommand, validationState);
-        if (type === 'ok') {
-            let [event,] = v;
-            render(commands.concat([[addItemCommand, event]]), events, {...uiState, newTaskName: ''});
-        } else {
-            console.log("ERROR", type, v);
-        }
-    };
-
-    let viewState = events.concat(commands.map(t => t[1])).reduce(
+    const state = store.getState();
+    console.log('state', state);
+    const viewState = state.events.concat(state.commands.map(t => t[1])).reduce(
         (viewState, event) => viewStateHandlers[event.type](event, viewState),
         emptyViewState
     );
-
+    const onNewTaskInput = (newTaskName) => store.dispatch(newTaskInput(newTaskName));
+    const onNewTaskSubmit = () => store.dispatch(newTaskSubmit());
     ReactDOM.render(
-        <App list={viewState} ui={uiState} onNewTaskInput={onNewTaskInput} onNewTaskSubmit={onNewTaskSubmit} />,
+        <App list={viewState} ui={state.uiState} onNewTaskInput={onNewTaskInput} onNewTaskSubmit={onNewTaskSubmit} />,
         document.getElementById('root')
     );
     console.timeEnd('render');
+};
+
+function update(state, action) {
+    console.log("UPDATE", state, action);
+    switch (action.type) {
+        case 'newTaskInput':
+            return {...state, uiState: {...state.uiState, newTaskName: action.newTaskName}};
+
+        case 'newTaskSubmit':
+            const validationState = state.events.reduce(
+                (validationState, event) => list.eventHandlers[event.type](event, validationState),
+                list.emptyState()
+            );
+            const addItemCommand = list.addItem({id: guid(), name: state.uiState.newTaskName});
+            const {type, v} = list.commandHandlers.addItem(addItemCommand, validationState);
+
+            if (type === 'ok') {
+                let [event,] = v;
+
+                return {
+                    ...state,
+                    uiState: {...state.uiState, newTaskName: ''},
+                    commands: state.commands.concat([[addItemCommand, event]])
+                };
+            } else {
+                return state;
+            }
+        default:
+            return state;
+    }
 }
 
 loadListEvents(listId, 1, 'latest').then((eventRecords) => {
-    render([], eventRecords.map(eventRecord => eventRecord.eventData), {});
+    const store = createStore(
+        update,
+        {
+            commands: [],
+            events: eventRecords.map(eventRecord => eventRecord.eventData),
+            latestVersion: eventRecords[eventRecords.length - 1].listVersion,
+            uiState: {}
+        }
+    );
+    store.subscribe(render.bind(null, store));
+    render(store);
 });
